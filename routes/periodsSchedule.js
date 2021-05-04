8817b9dd6aa819a237cec6e39239d472e1fb1909
Aditya Rana <official.adityarana01@gmail.com>
2021-05-04 10:46:40 +0530

    Added new route

--- routes/periodsSchedule.js
+++ routes/periodsSchedule.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getPeriodsSchedule, getFreeSlotsForBooking } = require('../controllers/periodsSchedule');
+const { getPeriodsSchedule, getFreeSlotsForBooking, bookSlot } = require('../controllers/periodsSchedule');
 
 /* GET home page. */
 router.get('/', getPeriodsSchedule);
 router.post('/free', getFreeSlotsForBooking);
+router.post('/book', bookSlot);
 
 module.exports = router;
