ab6c4ab469d2953d6dff806c5234cc9a6de8754c
Aditya Rana <official.adityarana01@gmail.com>
2021-05-05 23:44:04 +0530

    Automatic refactor

--- controllers/periodsSchedule.js
+++ controllers/periodsSchedule.js
@@ -1,16 +1,16 @@
 const weekModel = require('../models/weekModel');
 
 const findFreeSlots = daySchedule => {
-  if(daySchedule.length == 0) {
-    return [{start: 8, end: 17}];
+  if (daySchedule.length == 0) {
+    return [{ start: 8, end: 17 }];
   }
   let freeSlots = [];
-  if(daySchedule[0].start > 8) freeSlots.push({start: 8, end: daySchedule[0].start});
-  for(let i = 1; i < daySchedule.length; ++i) {
-    if(daySchedule[i].start > daySchedule[i - 1].end) freeSlots.push({start: daySchedule[i - 1].end, end: daySchedule[i].start});
+  if (daySchedule[0].start > 8) freeSlots.push({ start: 8, end: daySchedule[0].start });
+  for (let i = 1; i < daySchedule.length; ++i) {
+    if (daySchedule[i].start > daySchedule[i - 1].end) freeSlots.push({ start: daySchedule[i - 1].end, end: daySchedule[i].start });
   }
   const lastElement = daySchedule[daySchedule.length - 1];
-  if(lastElement.end < 17) freeSlots.push({start: lastElement.end, end: 17});
+  if (lastElement.end < 17) freeSlots.push({ start: lastElement.end, end: 17 });
   return freeSlots;
 }
 
@@ -18,8 +18,8 @@ const addFreeSlots = daySchedule => {
   let updatedSchedule = [];
   const freeSlots = findFreeSlots(daySchedule);
   let freeSlotIndex = 0, dayScheduleIndex = 0;
-  while(freeSlotIndex < freeSlots.length && dayScheduleIndex < daySchedule.length) {
-    if(freeSlots[freeSlotIndex].start < daySchedule[dayScheduleIndex].start) {
+  while (freeSlotIndex < freeSlots.length && dayScheduleIndex < daySchedule.length) {
+    if (freeSlots[freeSlotIndex].start < daySchedule[dayScheduleIndex].start) {
       updatedSchedule.push(freeSlots[freeSlotIndex]);
       freeSlotIndex++;
     }
@@ -29,11 +29,11 @@ const addFreeSlots = daySchedule => {
     }
   }
 
-  while(freeSlotIndex < freeSlots.length) {
+  while (freeSlotIndex < freeSlots.length) {
     updatedSchedule.push(freeSlots[freeSlotIndex]);
     freeSlotIndex++;
   }
-  while(dayScheduleIndex < daySchedule.length) {
+  while (dayScheduleIndex < daySchedule.length) {
     updatedSchedule.push(daySchedule[dayScheduleIndex]);
     dayScheduleIndex++;
   }
@@ -43,7 +43,7 @@ const addFreeSlots = daySchedule => {
 
 const preprocessTimeTable = data => {
   const periodsSchedule = [data.monday, data.tuesday, data.wednesday, data.thursday, data.friday];
-  for(let i = 0; i < 5; ++i) {
+  for (let i = 0; i < 5; ++i) {
     periodsSchedule[i] = addFreeSlots(periodsSchedule[i]);
   }
   return periodsSchedule;
@@ -61,7 +61,7 @@ const getPeriodsSchedule = (req, res, next) => {
 };
 
 const getDayFromNumber = num => {
-  switch(num) {
+  switch (num) {
     case 1: return "monday";
     case 2: return "tuesday";
     case 3: return "wednesday";
@@ -72,19 +72,19 @@ const getDayFromNumber = num => {
 
 const findAllFittingSlots = (slotsOfGivenDuration, durationWanted, slot) => {
   const duration = slot.end - slot.start;
-  if(duration < durationWanted) return;
-  if(duration == durationWanted) {
+  if (duration < durationWanted) return;
+  if (duration == durationWanted) {
     slotsOfGivenDuration.push(slot);
     return;
   }
-  slotsOfGivenDuration.push({start: slot.start, end: slot.start + durationWanted * 1}); // * 1 is intended to force JS to treat durationWanted as a number
-  const newSlot = {start: slot.start + 1, end: slot.end};
+  slotsOfGivenDuration.push({ start: slot.start, end: slot.start + durationWanted * 1 }); // * 1 is intended to force JS to treat durationWanted as a number
+  const newSlot = { start: slot.start + 1, end: slot.end };
   findAllFittingSlots(slotsOfGivenDuration, durationWanted, newSlot);
 }
 
 const findSlotsOfGivenDuration = (freeSlots, duration) => {
   let slotsOfGivenDuration = [];
-  for(let i = 0; i < freeSlots.length; ++i) {
+  for (let i = 0; i < freeSlots.length; ++i) {
     findAllFittingSlots(slotsOfGivenDuration, duration, freeSlots[i]);
   }
   return slotsOfGivenDuration;
@@ -96,16 +96,16 @@ const getFirstDayOfWeekFromDate = date => {
 
 const getWeekAndDayOfDate = (date) => {
   let weekId = 2;
-  if(getFirstDayOfWeekFromDate(date) == getFirstDayOfWeekFromDate(new Date())) weekId = 1;
+  if (getFirstDayOfWeekFromDate(date) == getFirstDayOfWeekFromDate(new Date())) weekId = 1;
 
   let dayOfSlot = getDayFromNumber(date.getDay());
-  return {weekId, dayOfSlot};
+  return { weekId, dayOfSlot };
 }
 
 const getFreeSlotsForBooking = (req, res, next) => {
   const slotDuration = req.body.slotDurationWanted;
   const slotDate = new Date(req.body.dateOfSlotWanted);
-  
+
   const { weekId, dayOfSlot } = getWeekAndDayOfDate(slotDate);
 
   weekModel.findOne({ weekId: weekId })
@@ -118,7 +118,7 @@ const getFreeSlotsForBooking = (req, res, next) => {
     })
     .then(jsonResponse => {
       res.send(jsonResponse);
-    });  
+    });
 }
 
 const compareByStartTime = (slot1, slot2) => {
@@ -136,7 +136,7 @@ const bookSlot = (req, res, next) => {
   const slotDate = new Date(req.body.dateOfSlotWanted);
   const slotTime = req.body.slotSelected;
   const duration = req.body.slotDurationWanted;
-  
+
   const { weekId, dayOfSlot } = getWeekAndDayOfDate(slotDate);
 
   const newSlot = {
@@ -148,22 +148,22 @@ const bookSlot = (req, res, next) => {
   weekModel.findOne({ weekId: weekId })
     .then(doc => {
       try {
-        doc[dayOfSlot].push(newSlot);        
+        doc[dayOfSlot].push(newSlot);
         doc[dayOfSlot].sort(compareByStartTime);
       }
-      catch(err) {
+      catch (err) {
         res.status(400);
         res.end();
       }
       doc.save()
-      .then(() => {
-        res.status(200);
-        res.end();
-      })
-      .catch(() => {
-        res.status(400);
-        res.end();
-      });
+        .then(() => {
+          res.status(200);
+          res.end();
+        })
+        .catch(() => {
+          res.status(400);
+          res.end();
+        });
     });
 }
 
@@ -173,28 +173,28 @@ const cancelSlot = (req, res, next) => {
   const { weekId, dayOfSlot } = getWeekAndDayOfDate(slotDate);
 
   weekModel.findOne({ weekId: weekId })
-  .then(doc => {
-    if(!doc) throw "Document not found";
-    const numOfPeriods = doc[dayOfSlot].length;
-    let indexOfPeriodWithGivenStart = 0;
-    for(; indexOfPeriodWithGivenStart < numOfPeriods; ++indexOfPeriodWithGivenStart) {
-      if(doc[dayOfSlot][indexOfPeriodWithGivenStart].start == startTime) break;
-    }
-    if(indexOfPeriodWithGivenStart == numOfPeriods) {
+    .then(doc => {
+      if (!doc) throw "Document not found";
+      const numOfPeriods = doc[dayOfSlot].length;
+      let indexOfPeriodWithGivenStart = 0;
+      for (; indexOfPeriodWithGivenStart < numOfPeriods; ++indexOfPeriodWithGivenStart) {
+        if (doc[dayOfSlot][indexOfPeriodWithGivenStart].start == startTime) break;
+      }
+      if (indexOfPeriodWithGivenStart == numOfPeriods) {
+        res.status(400);
+        res.end();
+      }
+      else {
+        doc[dayOfSlot].splice(indexOfPeriodWithGivenStart, 1);
+        doc.save();
+        res.status(200);
+        res.end();
+      }
+    })
+    .catch(() => {
       res.status(400);
       res.end();
-    }
-    else {
-      doc[dayOfSlot].splice(indexOfPeriodWithGivenStart, 1);
-      doc.save();
-      res.status(200);
-      res.end();
-    }
-  })
-  .catch(() => {
-    res.status(400);
-    res.end();
-  });
+    });
 }
 
 module.exports = {
