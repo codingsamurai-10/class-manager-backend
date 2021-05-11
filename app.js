d9cef49533bde7048b623730e17b01307e418d45
Aditya Rana <official.adityarana01@gmail.com>
2021-05-12 03:57:36 +0530

    Removed cors for deploy

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ require('./db.js');
 const passportSetup = require('./config/passport-setup');
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
