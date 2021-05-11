f4dc7e535c93e6c4d7a9025e67a495eb1f34d7ea
Aditya Rana <official.adityarana01@gmail.com>
2021-05-12 03:57:09 +0530

    Changed hardcoded key into secret

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieSession({
-    keys: ['ABC'],
+    keys: [process.env.COOKIE_KEY],
     maxAge: 30 * 24 * 60 * 60 * 1000,
 }));
 
