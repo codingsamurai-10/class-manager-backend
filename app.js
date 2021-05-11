17732972ec01b3b0923fa64f377c619cc3268d47
Aditya Rana <official.adityarana01@gmail.com>
2021-05-12 01:14:21 +0530

    Renamed API endpoints

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+
+require('dotenv').config();
+require('./db.js');
+
+const passportSetup = require('./config/passport-setup');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieSession({
+    keys: ['ABC'],
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const periodsScheduleRouter = require('./routes/periodsSchedule');
 const authRouter = require('./routes/auth');
@@ -12,32 +32,9 @@ const profileRouter = require('./routes/profile');
 const notifRouter = require('./routes/notif');
 const changeWeekId = require('./scheduleWeekIdChange.js');
 
-require('dotenv').config();
-require('./db.js');
-
-const app = express();
-app.use(cors());
-
-app.use(cookieParser());
-app.use(cookieSession({
-    keys: [process.env.COOKIE_KEY],
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-const passportSetup = require('./config/passport-setup');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/periodsSchedule', periodsScheduleRouter);
-app.use('/auth', authRouter);
-app.use('/profile', profileRouter);
-app.use('/Notifications', notifRouter)
+app.use('/api/periodsSchedule', periodsScheduleRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/Notifications', notifRouter)
 
 module.exports = app;
